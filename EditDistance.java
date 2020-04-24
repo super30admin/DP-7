bf6aa664f33d6a007ccc246cda9769dc55447c2b
BillBaquerizo <billbz9@gmail.com>
2020-04-23 21:24:00 -0400

    Completed DP-7. Please check.

---
+++ EditDistance.java
@@ -0,0 +1,27 @@
+// Time Complexity: O(M*N)
+// Space Complexity: O(M*N)
+
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int[][] dp=new int[word1.length()+1][word2.length()+1];
+        
+        for(int i=0;i<= word1.length();i++){
+            dp[i][0] = i;
+        }
+        for(int j=0;j<=word2.length();j++){
+            dp[0][j] = j;
+        }
+        
+        for(int m=1;m<=word1.length();m++){
+            for(int n=1;n<=word2.length();n++){
+                if(word1.charAt(m-1)==word2.charAt(n-1)){
+                dp[m][n]=dp[m-1][n-1];
+                    }
+                    else{
+                        dp[m][n]=1+Math.min(Math.min(dp[m-1][n],dp[m][n-1]),dp[m-1][n-1]);
+                    }
+            }
+        }
+        return dp[word1.length()][word2.length()];
+    }
+}
