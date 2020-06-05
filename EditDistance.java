c3c1d17bf47e854d5736ae6851845b89dcd79054
RutujaKaushike <rutujakaushike@gmail.com>
2020-06-05 03:17:38 -0500

    Updated code

--- EditDistance.java
+++ EditDistance.java
@@ -68,4 +68,33 @@ public class EditDistance {
         System.out.println(minDistanceBF("abc",
                 "xyz"));
     }
+
+    // Taken in class, easiest
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m+1][n+1];
+
+        // Inititalizing "" wrt word2
+        for(int i = 1; i <= n; i++){
+            dp[0][i] = i;
+        }
+
+        // Inititalizing "" wrt word1
+        for(int i = 1; i <= m; i++){
+            dp[i][0] = i;
+        }
+
+        // two conditions, if two chars are same, copy from last pair of words, i.e. diagonals, else take min of prev three i.e. up, corner, prev
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i-1][j], Math.min(dp[i-1][j-1], dp[i][j-1]));
+                }
+            }
+        }
+        return dp[m][n];
+    }
 }
--- RegularExpressionMatching.java
+++ RegularExpressionMatching.java
@@ -22,7 +22,7 @@ public class RegularExpressionMatching {
     // Dynamic Programming
     boolean[][] dp;
 
-    public boolean isMatch(String s, String p) {
+    public boolean isMatchDP(String s, String p) {
         dp = new boolean[s.length() + 1][p.length() + 1];
         return helper(0,0,s,p);
     }
@@ -42,6 +42,49 @@ public class RegularExpressionMatching {
 
         dp[i][j] = ans;
         return ans;
+    }
+
+    // Class, Best Algorithm
+
+    public boolean isMatch(String s, String p) {
+        if(p == null){
+            return s==null;
+        }
+
+        int pLength = p.length();
+        int sLength = s.length();
+
+        boolean[][] dp = new boolean[sLength+1][pLength+1];
+
+        dp[0][0] = true;
+
+        // if pattern consists of *, go 2 steps back and copy the value
+        for(int i = 1; i < dp[0].length; i++){
+            if(p.charAt(i-1) == '*'){
+                dp[0][i] = dp[0][i-2];
+            }
+        }
+
+        for(int i = 1; i < dp.length; i++){
+            for(int j = 1; j < dp[0].length; j++){
+                // equal chars and not * , I have a character
+                if(p.charAt(j-1) == s.charAt(i-1) || p.charAt(j-1) == '.'){
+                    dp[i][j] = dp[i-1][j-1];
+                } else if(p.charAt(j-1) == '*'){
+                    // there is a *
+                    // zero case
+                    dp[i][j] = dp[i][j-2];
+
+                    // one case
+                    if(s.charAt(i-1) == p.charAt(j-2) || p.charAt(j-2) == '.'){
+                        if(dp[i][j] || dp[i-1][j]){
+                            dp[i][j] = true;
+                        }
+                    }
+                }
+            }
+        }
+        return dp[sLength][pLength];
     }
 
 }
