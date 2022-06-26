06315ceb712bd22b7399d662b2bcb6903d132660
PrakharSharma35 <102261502+PrakharSharma35@users.noreply.github.com>
2022-06-25 19:18:53 -0500

    DP 7 submission

---
+++ ExpressionMatching.java
@@ -0,0 +1,33 @@
+//TC : O(mxn)
+//SC : O(mxn)
+class Solution {
+    public int minDistance(String word1, String word2) {
+        if(word1.equals(word2)) return 0;
+        int m = word1.length()+1;
+        int n = word2.length()+1;
+        int[][] dp = new int[m][n];
+        dp[0][0] = 0;
+
+        for(int i=1;i<m;i++)
+            dp[i][0] = i;
+
+        for(int i=1;i<n;i++)
+            dp[0][i] = i;
+
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                if(word1.charAt(i-1) == word2.charAt(j-1))
+                {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = Math.min(dp[i][j-1],Math.min(dp[i-1][j-1],dp[i-1][j]))+1;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
