862de501da35d0906eb258b5049e9b32e7bfe7a3
Neha Mishra <neha79091@gmail.com>
2022-07-17 09:21:17 +0530

    DP7Done

---
+++ RegularExpressionMatching.java
@@ -0,0 +1,43 @@
+// Time Complexity :O(mn)where m is length of p string and n is length of s string
+// Space Complexity :O(mn)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :no
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int slen = s.length();
+        int plen = p.length();
+        boolean[][] dp = new boolean[slen + 1][plen + 1];
+        dp[0][0] = true;
+        // filling first row
+        for (int i = 1; i < plen + 1; i++) {
+            if (p.charAt(i - 1) == '*') {
+                dp[0][i] = dp[0][i - 2];
+            }
+        }
+        //
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                // char case
+                if (p.charAt(j - 1) != '*') {
+                    // check prev character
+                    if (p.charAt(j - 1) == s.charAt(i - 1) || p.charAt(j - 1) == '.') {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    }
+                }
+                // star case
+                else {
+                    // zero case
+                    dp[i][j] = dp[i][j - 2];
+                    // one case
+                    if (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.') {
+                        dp[i][j] = dp[i - 1][j] || dp[i][j];
+                    }
+
+                }
+                // character case
+
+            }
+        }
+        return dp[slen][plen];
+    }
+}
