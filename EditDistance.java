25ee2f3ac188c9a5a59f13fcc557e08a642cfcf7
Shivani31996 <shivani.chavan96@gmail.com>
2022-12-25 15:01:19 -0500

    Please review

--- EditDistance.java
+++ EditDistance.java
@@ -46,3 +46,41 @@ public class EditDistance {
         return dp[m][n];
     }
 }
+
+
+//Optimization - Using a single dimensional array
+class Solution {
+    public int minDistance(String word1, String word2) {
+        if(word1.equals(word2))
+            return 0;
+        
+        int n = word1.length();
+        int m = word2.length();
+        int dp[] = new int[n+1];
+        
+        for(int j=0; j<=n; j++)
+        {
+            dp[j] = j;
+        }
+        
+        for(int i=1; i<=m; i++)
+        {
+            int diagUp = dp[0];
+            dp[0] = i;
+            for(int j=1; j<=n; j++)
+            {
+                int temp = dp[j];
+                if(word1.charAt(j-1) == word2.charAt(i-1))
+                {
+                    dp[j] = diagUp;
+                }
+                else
+                {
+                    dp[j] = 1 + Math.min(diagUp,Math.min(dp[j-1],dp[j]));
+                }
+                diagUp = temp;
+            }
+        }
+        return dp[n];
+    }
+}
