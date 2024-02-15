e3e89d3f44692706ad762ec05239954f2fc43b76
SuprajaGandhi <137209418+SuprajaGandhi@users.noreply.github.com>
2024-02-15 14:21:19 -0700

    Update MinDistance.py

--- MinDistance.py
+++ MinDistance.py
@@ -1,10 +1,5 @@
 #TC: O(m+1*n+1)
 #SC: O(m+1*n+1)
-#This is same as the regex problem. Here the return type is integer so, we fill the table with 
-#numbers unlike true or false previously. In insertion, we will insert the character to be matched. 
-#In edit, replace h with r, -r cancels -r and becomes - and -1. Which is the diagonal left element
-#In insert, insert h with r, so -hr and -r, cancel r and the result is at the left element
-#In delete, delete h for r , so -  and -r, which is the top element
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         m=len(word1)
